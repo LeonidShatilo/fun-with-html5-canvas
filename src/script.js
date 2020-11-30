8f41a6351945bce6dffd38145d6963e960b7efa7
Leonid Shatilo <shatiloleonid@gmail.com>
2020-11-30 19:51:55 +0300

    feat: add script

---
+++ src/script.js
@@ -0,0 +1,44 @@
+const canvas = document.querySelector('#draw'),
+      context = canvas.getContext('2d');
+canvas.width = window.innerWidth; // Browser window width
+canvas.height = window.innerHeight; // Browser window height
+context.lineJoin = 'round'; // Angle type created by intersection of two lines
+context.lineCap = 'round'; // End of line style
+context.lineWidth = 100; // Line width
+
+let isDrawing = false,
+    isAccrual = true;
+
+let lastX = 0,
+    lastY = 0,
+    colorTone = 0;
+
+function draw(e) {
+  if (!isDrawing) return; // Stop the function from running when the mouse is up
+  context.strokeStyle = `hsl(${colorTone}, 100%, 50%)`;
+  context.beginPath();
+  context.moveTo(lastX, lastY); // Start from
+  context.lineTo(e.offsetX, e.offsetY); // Go to
+  context.stroke(); // Draws a figure by parameters above
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  colorTone++; // Change the color tone
+  if (colorTone >= 360) {
+    colorTone = 0;
+  }
+  if (context.lineWidth >= 100 || context.lineWidth <= 10) {
+    isAccrual = !isAccrual;
+  }
+  if (isAccrual) {
+    context.lineWidth++;
+  } else {
+    context.lineWidth--;
+  }
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
