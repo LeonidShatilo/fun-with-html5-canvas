7b62052d808a50a8ce71457aae0d063edd971d2d
Leonid Shatilo <shatiloleonid@gmail.com>
2020-11-30 21:06:37 +0300

    refactor: add comments

--- src/script.js
+++ src/script.js
@@ -7,20 +7,19 @@ context.lineCap = 'round'; // End of line style
 context.lineWidth = 100; // Line width
 
 let isDrawing = false,
-    isAccrual = true;
-
-let lastX = 0,
+    isAccrual = true,
+    lastX = 0,
     lastY = 0,
     colorTone = 0;
 
 function draw(e) {
   if (!isDrawing) return; // Stop the function from running when the mouse is up
   context.strokeStyle = `hsl(${colorTone}, 100%, 50%)`;
-  context.beginPath();
+  context.beginPath(); // Begin path
   context.moveTo(lastX, lastY); // Start from
   context.lineTo(e.offsetX, e.offsetY); // Go to
   context.stroke(); // Draws a figure by parameters above
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // Last coordinates update
   colorTone++; // Change the color tone
   if (colorTone >= 360) {
     colorTone = 0;
@@ -38,7 +37,7 @@ function draw(e) {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // Last coordinates update
 });
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
